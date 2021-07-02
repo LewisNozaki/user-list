a916b5d442399bd392cbd6acaacc41d236a3b7cf
Lewis Nozaki <lhnozaki@outlook.com>
2021-07-01 17:18:09 -1000

    updates

--- src/components/Users/AddUserForm.js
+++ src/components/Users/AddUserForm.js
@@ -4,22 +4,22 @@ const AddUserForm = () => {
   const [ username, setUsername ] = useState("");
 
   const handleInputChange = e => setUsername(e.target.value);
-
+  
   const handleSubmit = e => {
     e.preventDefault();
 
     console.log(username);
   }
-
+  
   return (
     <form onSubmit={handleSubmit}>
-      <label
-        htmlFor="username">username:</label>
+      <label htmlFor="username">username:</label>
       <input 
         id="username"
         name="username"
         type="text" 
-        onChange={handleInputChange}/>
+        onChange={handleInputChange} 
+      />
       <button type="submit">Add</button>
     </form>
   )
