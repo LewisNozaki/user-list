53d33afe0c0c6b4d13cc958ddda5bfdc926eef58
Lewis Nozaki <lhnozaki@outlook.com>
2021-07-09 12:09:32 -1000

    updates

--- src/components/Helpers/Wrapper.js
+++ src/components/Helpers/Wrapper.js
@@ -3,3 +3,8 @@ const Wrapper = ({ children }) => {
 };
 
 export default Wrapper;
+
+// This is a custom react wrapper. We can use it the same way we use a react fragment. 
+// We just have to create the custom wrapper like this and import it into our component.
+// Then we wrap this element around our other elements. 
+// It works exactly like React.Fragment></React.Fragment> or the shorthand <></>
